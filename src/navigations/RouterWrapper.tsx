9465eac46f56343db6ba4d17d37b7e6fe0ef3917
OrazovTairlan <orazovtairlan@gmail.com>
2022-04-07 07:41:22 +0600

    Added RouterWrapper component.
    It handles all routes by users.

---
+++ src/navigations/RouterWrapper.tsx
@@ -0,0 +1,39 @@
+import React, {lazy, useContext} from 'react';
+import {Navigate, Route, Routes, unstable_HistoryRouter as HistoryRouter} from 'react-router-dom';
+import {RootStoreContext} from "../store/rootStore";
+import {observer} from "mobx-react";
+import Loadable from "../components/Loadable/Loadable";
+import {history} from "../utils/history";
+import UserService from "../services/User/UserService";
+import Error from "../pages/Error";
+import PageNotFound from "../pages/PageNotFound";
+import {URLS} from "../utils/common";
+
+const SitesLayout = Loadable(lazy(() => import('../pages/SitesLayout')));
+const LoginLayout = Loadable(lazy(() => import('../pages/LoginLayout')));
+
+const RouterWrapper = () => {
+    const rootStore = useContext(RootStoreContext);
+    const {changeStatusToken} = rootStore.UserStore;
+    if (UserService.getToken("accessToken")) {
+        changeStatusToken(true);
+    }
+    return (
+        <HistoryRouter history={history}>
+            <div className="p-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen">
+                <Routes>
+                    <Route path={URLS.login}
+                           element={rootStore.UserStore.hasToken ? <Navigate to={"/views"}/> : <LoginLayout/>}/>
+                    <Route path={URLS.error}
+                           element={<Error/>}/>
+                    <Route path={URLS.views}
+                           element={rootStore.UserStore.hasToken ? <SitesLayout/> : <Navigate to={"/login"}/>}/>
+                    <Route path={URLS.pageNotFound}
+                           element={<PageNotFound/>}/>
+                </Routes>
+            </div>
+        </HistoryRouter>
+    );
+};
+
+export default observer(RouterWrapper);
