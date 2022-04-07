eee81252b708ada0433749258bf4667606a06015
OrazovTairlan <orazovtairlan@gmail.com>
2022-04-07 07:15:46 +0600

    update Relay types

--- src/RelayEnvironment.ts
+++ src/RelayEnvironment.ts
@@ -1,12 +1,13 @@
 // your-app-name/src/RelayEnvironment.js
+// @ts-ignore
 import {Environment, Network, RecordSource, Store} from 'relay-runtime';
-import fetchGraphQL from './src/fetchGraphQL';
-
+import fetchGraphQL from './fetchGraphQL';
+import {Variables} from "graphql-request";
+import {RequestParameters} from "relay-runtime/lib/util/RelayConcreteNode";
 // Relay passes a "params" object with the query name and text. So we define a helper function
 // to call our fetchGraphQL utility with params.text.
-async function fetchRelay(params, variables) {
-    const result = await fetchGraphQL(params.text, variables);
-    return result;
+async function fetchRelay(params:RequestParameters, variables:Variables) {
+   return  await fetchGraphQL(params.text, variables);
 }
 
 // Export a singleton instance of Relay Environment configured with our network function:
