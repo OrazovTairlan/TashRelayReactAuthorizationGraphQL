b8b2a170d10d6d8a3260105373724e82c36d2f5f
OrazovTairlan <orazovtairlan@gmail.com>
2022-04-07 07:39:51 +0600

    Added PageNotFound component. Rv6 will catch unnamed routes and return this component

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return (
+        <div
+            className="
+    flex
+    items-center
+    justify-center
+    overflow-hidden
+    bg-transparent
+    h-screen
+    fixed
+    top-0
+    left-0
+    right-0
+    bottom-0
+    bg-transparent
+  "
+        >
+            <div className="px-40 py-20 bg-transparent rounded-md shadow-xl">
+                <div className="flex flex-col items-center">
+                    <h1 className="font-bold text-blue-600 text-9xl">404</h1>
+
+                    <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+                        <span className="text-red-500">Oops!</span> Page not found
+                    </h6>
+
+                    <p className="mb-8 text-center text-gray-500 md:text-lg">
+                        
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PageNotFound;
+
+
