292184fc8cfa342ea98b017e8dbb31775fe0e7b0
OrazovTairlan <orazovtairlan@gmail.com>
2022-04-07 07:54:11 +0600

    added default route /

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -3,6 +3,7 @@ export const INVALID_TOKEN = "INVALID_TOKEN";
 export const API_URL = "https://tsarka-frontend-test.herokuapp.com/frontend/task/graphql";
 
 export const URLS = {
+    main: "/",
     login: "/login",
     views: "/views",
     error: "/error",
